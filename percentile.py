af040087a5f4901796866137716503095dfdce9b
luqni <mluqnibaehaqi@gmail.com>
2020-09-08 10:21:43 +0700

    first commit

---
+++ percentile.py
@@ -0,0 +1,7 @@
+import numpy
+
+ages = [2,5,6,7,8,11,15]
+
+x = numpy.percentile(ages, 10)
+
+print(x)
---
+++ simpangan_baku.py
@@ -0,0 +1,7 @@
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
